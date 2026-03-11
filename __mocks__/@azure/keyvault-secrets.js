95298fab2a859757c0dd50e723a909dc3c63249e
Michael Shimmins <1013281+shimms@users.noreply.github.com>
2026-03-11 18:13:15 +1000

    fix: resolve lint and prettier formatting errors

--- __mocks__/@azure/keyvault-secrets.js
+++ __mocks__/@azure/keyvault-secrets.js
@@ -1,8 +1,8 @@
 const SecretClient = jest.fn(() => ({
-  getSecret: jest.fn(() => Promise.resolve({ value: '', properties: { tags: {} } })),
-  setSecret: jest.fn(() =>
-    Promise.resolve({ properties: { version: '1' } })
+  getSecret: jest.fn(() =>
+    Promise.resolve({ value: '', properties: { tags: {} } })
   ),
+  setSecret: jest.fn(() => Promise.resolve({ properties: { version: '1' } })),
   beginDeleteSecret: jest.fn(() =>
     Promise.resolve({ pollUntilDone: () => Promise.resolve() })
   ),
