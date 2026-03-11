2ecaa0ae0c6cd13222944296c57f9bb85a30df03
Michael Shimmins <1013281+shimms@users.noreply.github.com>
2026-03-12 06:55:17 +1000

    chore: migrate AWS SDK from v2 to v3 (#1)
    Replace monolithic aws-sdk v2 with modular v3 packages:
    - @aws-sdk/client-ssm
    - @aws-sdk/client-cloudformation
    - @aws-sdk/client-sts
    
    Removes unused S3, KMS, SNS, and SecretsManager mocks.

---
+++ __mocks__/@aws-sdk/client-cloudformation.js
@@ -0,0 +1,16 @@
+const mockSend = jest.fn(() => Promise.resolve());
+
+const CloudFormationClient = jest.fn(() => ({
+  send: mockSend
+}));
+
+const DescribeStacksCommand = jest.fn(input => ({
+  _commandName: 'DescribeStacksCommand',
+  input
+}));
+
+module.exports = {
+  CloudFormationClient,
+  DescribeStacksCommand,
+  __mockSend: mockSend
+};
---
+++ __mocks__/@aws-sdk/client-sts.js
@@ -0,0 +1,22 @@
+const mockSend = jest.fn(() =>
+  Promise.resolve({
+    Account: '1234',
+    Arn: 'mock-arn',
+    UserId: 'mock-user-id'
+  })
+);
+
+const STSClient = jest.fn(() => ({
+  send: mockSend
+}));
+
+const GetCallerIdentityCommand = jest.fn(input => ({
+  _commandName: 'GetCallerIdentityCommand',
+  input
+}));
+
+module.exports = {
+  STSClient,
+  GetCallerIdentityCommand,
+  __mockSend: mockSend
+};
