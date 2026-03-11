c26fbc442a69492b358a124947aa88356db8a0cc
Michael Shimmins <1013281+shimms@users.noreply.github.com>
2026-03-11 20:43:07 +1000

    chore: migrate AWS SDK from v2 to v3
    Replace monolithic aws-sdk v2 with modular v3 packages:
    - @aws-sdk/client-ssm
    - @aws-sdk/client-cloudformation
    - @aws-sdk/client-sts
    
    Removes unused S3, KMS, SNS, and SecretsManager mocks.

---
+++ __mocks__/@aws-sdk/client-ssm.js
@@ -0,0 +1,34 @@
+const mockSend = jest.fn();
+
+const SSMClient = jest.fn(() => ({
+  send: mockSend
+}));
+
+const GetParametersCommand = jest.fn(input => ({
+  commandName: 'GetParametersCommand',
+  input
+}));
+
+const GetParametersByPathCommand = jest.fn(input => ({
+  commandName: 'GetParametersByPathCommand',
+  input
+}));
+
+const PutParameterCommand = jest.fn(input => ({
+  commandName: 'PutParameterCommand',
+  input
+}));
+
+const DeleteParametersCommand = jest.fn(input => ({
+  commandName: 'DeleteParametersCommand',
+  input
+}));
+
+module.exports = {
+  SSMClient,
+  GetParametersCommand,
+  GetParametersByPathCommand,
+  PutParameterCommand,
+  DeleteParametersCommand,
+  __mockSend: mockSend
+};
